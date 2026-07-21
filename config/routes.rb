31621dff42974903d8404eae54a83362e9ce0879
suzu727991 <kensuzuki@fujisanba.com>
2026-07-21 20:09:55 +0900

    商品削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   root "items#index"
 end
